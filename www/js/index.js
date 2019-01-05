9c4d026d833ccdf1f3f498a0583252d0db91d566
Anas Mirza <anasmirza534@gmail.com>
2019-01-06 01:41:05 +0530

    add debug alert

--- www/js/index.js
+++ www/js/index.js
@@ -56,7 +56,7 @@ var app = {
         push.on('registration', function(data) {
             console.log('registration event: ' + data.registrationId);
             // myAlert('on register', data.registrationId);
-            alert('hey', JSON.stringify(data));
+            alert('on register: ' + JSON.stringify(data));
 
             var oldRegId = localStorage.getItem('registrationId');
             if (oldRegId !== data.registrationId) {
